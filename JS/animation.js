1fd50290deb7fd366086386b97e428f625a3cb76
stiven <brayan-steven23@hotmail.com>
2023-07-07 22:39:45 -0500

    creacion de pagina estatica segun enseñanzas y conocimientos propios

---
+++ JS/animation.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.hamburger', {
+        duration: 2000, 
+        origin: 'left',
+        distance: '-300px'
+    });
+
+    sr.reveal('.header-txt', {
+        duration: 2000, 
+        origin: 'right',
+        distance: '-300px'
+    });
+
+    sr.reveal('.product', {
+        duration: 2000, 
+        origin: 'right',
+        distance: '300px'
+    });
+
+    sr.reveal('.menu-nav', {
+        duration: 2000, 
+        origin: 'right',
+        distance: '300px'
+    });
+
+    sr.reveal('.info-content', {
+        duration: 2000, 
+        origin: 'left',
+        distance: '300px'
+    });
